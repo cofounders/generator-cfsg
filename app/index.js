2b2960519c47d94fc08151ce5e437baa89f36b9b
Sebastiaan Deckers <seb@ninja.sg>
2013-10-16 22:37:14 +0800

    Make the name configurable via templates and input prompt or command line argument

--- app/index.js
+++ app/index.js
@@ -2,10 +2,12 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
-
+var fs = require('fs');
 
 var CfsgGenerator = module.exports = function CfsgGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
+
+  this.argument('appname', { type: String, required: false });
 
   this.on('end', function () {
     this.installDependencies({
@@ -26,19 +28,43 @@ CfsgGenerator.prototype.askFor = function askFor() {
   // have Yeoman greet the user.
   console.log(this.yeoman);
 
-  var prompts = [{
-    name: 'projectName',
-    message: 'What is the name of the project?',
-    default: 'Hello World'
-  }];
+  var prompts = [];
+
+  if (!this.appname) {
+    prompts.push({
+      name: 'appname',
+      message: 'What is the name of the project?',
+      default: 'My App'
+    });
+  }
 
   this.prompt(prompts, function (props) {
-    this.projectName = props.projectName;
+    this.appname = props.appname || this.appname;
 
     cb();
   }.bind(this));
 };
 
 CfsgGenerator.prototype.app = function app() {
-  this.directory('.', '.');
+  var sourceRoot = this.sourceRoot();
+  var sourceFiles = this.expand(path.join(sourceRoot, '**/*'));
+  var templatePrefix = '_';
+  sourceFiles.forEach(function (sourcePath) {
+    var relativePath = path.relative(sourceRoot, sourcePath);
+    var stats = fs.statSync(sourcePath);
+    if (stats.isDirectory()) {
+      this.mkdir(relativePath);
+    } else {
+      var fileName = path.basename(relativePath);
+      if (fileName.charAt(0) === templatePrefix) {
+        var destination = path.join(
+          path.dirname(relativePath),
+          path.basename(relativePath).substr(1)
+        );
+        this.template(relativePath, destination);
+      } else {
+        this.copy(relativePath, relativePath);
+      }
+    }
+  }.bind(this));
 };
--- app/templates/_README.md
+++ app/templates/_README.md
@@ -1,4 +1,4 @@
-# My App
+# <%= _.escapeHTML(appname) %>
 
 ## The Build
 
