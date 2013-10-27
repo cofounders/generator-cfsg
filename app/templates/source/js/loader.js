2b61e6a4a7b71483b483f40fcb7d64e2a446e563
Sebastiaan Deckers <seb@ninja.sg>
2013-10-27 23:06:25 +0800

    Better readability for RequireJS dependency URLs by not splitting into lines

--- app/templates/source/js/loader.js
+++ app/templates/source/js/loader.js
@@ -1,23 +1,14 @@
+/*jslint maxlen: 240 */
 require.config({
-
 	baseUrl: '/js',
-
 	deps: ['main'],
-
 	paths: {
-		backbone: '//cdnjs.cloudflare.com' +
-			'/ajax/libs/backbone.js/1.0.0/backbone-min',
-		handlebars: '//cdnjs.cloudflare.com' +
-			'/ajax/libs/handlebars.js/1.0.0-rc.4/handlebars.min',
-		jquery: '//cdnjs.cloudflare.com' +
-			'/ajax/libs/jquery/1.9.1/jquery.min',
-		'backbone.layoutmanager': '//cdnjs.cloudflare.com' +
-			'/ajax/libs/backbone.layoutmanager' +
-			'/0.8.8/backbone.layoutmanager.min',
-		underscore: '//cdnjs.cloudflare.com' +
-			'/ajax/libs/underscore.js/1.4.4/underscore-min'
+		backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min',
+		handlebars: '//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0-rc.4/handlebars.min',
+		jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min',
+		'backbone.layoutmanager': '//cdnjs.cloudflare.com/ajax/libs/backbone.layoutmanager/0.8.8/backbone.layoutmanager.min',
+		underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min'
 	},
-
 	shim: {
 		backbone: {
 			deps: ['underscore', 'jquery'],
@@ -36,5 +27,4 @@ require.config({
 			exports: '_'
 		}
 	}
-
 });
