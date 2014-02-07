ec710e830a08b0951f195b4f5d913c0ba2e06f1a
Sebastiaan Deckers <seb@ninja.sg>
2014-02-07 22:23:46 +0800

    Fixed typo

--- mvc/templates/view.js
+++ mvc/templates/view.js
@@ -1,5 +1,5 @@
 	Views.<%= _.classify(name) %> = Backbone.View.extend({
-		template: '_.slugify(_.humanize(module)) %>/<%= _.slugify(_.humanize(name)) %>',
+		template: '<%= _.slugify(_.humanize(module)) %>/<%= _.slugify(_.humanize(name)) %>',
 		initialize: function (options) {
 			this.options = options;
 		},
